776c9eded8fc4e26f7e5ef2890e7a636a3712eb8
Lary David <larydavid34@gmail.com>
2026-02-20 17:34:02 +0100

    Feat: se ha agregado el método testConnection()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,46 @@
 package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Connection con = null;
+    record Paciente(int id, String nombre, String especie, int edad, String propietario) {}
+    static ArrayList<Paciente> listaPacientes = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args){
+
+        System.out.println("SiGCVet conectado");
+        testConnection();
+        cerrarConexion();
+    }
+
+    public static void testConnection() {
+        String url = "jdbc:postgresql://ep-lively-sunset-aby2qoj7-pooler.eu-west-2.aws.neon.tech:5432/proyecto_alumno7?sslmode=require";
+        String usuario = "neondb_owner";
+        String password = "npg_3FCiZhx7VnBo";
+        System.out.println("Intentando conectar a la base de datos...");
+        try {
+            con = DriverManager.getConnection(url, usuario, password);
+            if (con != null && !con.isClosed()) {
+                System.out.println("Conexión establecida con quim_test_db");
+            }
+        } catch (SQLException e) {
+            System.err.println("ERROR de conexiÛn: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    public static void cerrarConexion() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+                System.out.println("Conexión establecida con quim_test_db");
+            }
+        } catch (SQLException e) {
+            System.err.println("No se ha cerrado correctamente la base de datos" + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
